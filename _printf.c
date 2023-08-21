07f219e3f8d95980088ac0b84445e45109135b38
Ephraim4774 <ephikhomo@gmail.com>
2023-08-21 01:48:14 -0700

    a function that produces output according to a format

---
+++ _printf.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * my_printf: printf function
+ * main: main fuction
+ */
+
+void my_printf(const char *format, ...)
+{
+va_list args;
+va_start(args, format);
+
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+
+switch (*format)
+{
+case 'c':
+	{
+char c = (char) va_arg(args, int);
+putchar(c);
+break;
+}
+case 'd':
+{
+int d = va_arg(args, int);
+printf("%d", d);
+break;
+}
+case 's':
+{
+char *s = va_arg(args, char *);
+printf("%s", s);
+break;
+}
+default:
+putchar(*format);
+}
+}
+else
+{
+putchar(*format);
+}
+format++;
+}
+va_end(args);
+}
+int main(void)
+{
+int num = 42;
+char letter = 'A';
+char string[] = "Hello, world!";
+my_printf("Number: %d\nCharacter: %c\nString: %s\n", num, letter, string);
+return (0);
+}
