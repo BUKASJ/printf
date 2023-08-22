c51013c32f6cec5ab119d006fb5ec94a21ac29dd
BUKASJ <chukwuebukaedej@gmail.com>
2023-08-21 19:34:24 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -9,54 +9,57 @@
 
 int _putchar(char c)
 {
-return write(1, &c, 1);
+	return write(1, &c, 1);
 }
+
 int _printf(const char *format, ...)
 {
-va_list args;
-va_start(args, format);
-int count = 0;
-const char *c = format;
-while (*c != '\0')
-{
-if (*c == '%')
-{
-c++;
-if (*c == 'c')
-{
-char ch = va_arg(args, int);
-_putchar(ch);
-count++;
-}
-else if (*c == 's')
-{
-char *str = va_arg(args, char *);
-for (int i = 0; str[i] != '\0'; i++)
-{
-_putchar(str[i]);
-count++;
-}
-}
-else if (*c == '%')
-{
-_putchar('%');
-count++;
-}
-}
-else
-{
-_putchar(*c);
-count++;
-}
-c++;
-}
-va_end(args);
-return count;
+	int count = 0;
+	int i;
+	const char *c = format;
+	va_list args;
+	va_start(args, format);
+
+	while (*c != '\0')
+	{
+		if (*c == '%')
+		{
+			c++;
+			if (*c == 'c')
+			{
+				char ch = va_arg(args, int);
+				_putchar(ch);
+				count++;
+			}
+			else if (*c == 's')
+			{
+				char *str = va_arg(args, char *);
+				for (i = 0; str[i] != '\0'; i++)
+				{
+					_putchar(str[i]);
+					count++;
+				}
+			}
+			else if (*c == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+		}
+		else
+		{
+			_putchar(*c);
+			count++;
+		}
+		c++;
+	}
+	va_end(args);
+	return (count);
 }
 int main()
 {
 _printf("Hello, %s!\n", "world");
 _printf("Character: %c\n", 'A');
 _printf("Escaped percent: %%\n");
-return 0;
+return (0);
 }
--- handle_flag_characters.c
+++ handle_flag_characters.c
@@ -3,36 +3,37 @@
 
 void _printf(const char *format, ...)
 {
-va_list args;
-var_start(args, format);
+	va_list args;
+	va_start(args, format);
 
-while (*format != '\0')
-{
-if (*format == '%')
-{
-format++;
-if (*format == 'd' || *format == 'i')
-{
-int value = var_arg(args, int);
-printf("%d", value);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'd' || *format == 'i')
+			{
+				int value = va_arg(args, int);
+				printf("%d", value);
+			}
+			else
+			{
+				putchar(*format);
+			}
+		}
+		else
+		{
+			putchar(*format);
+		}
+		format++;
+	}
+	va_end(args);
 }
-else
-{
-putchar(*format);
-}
-}
-else
-{
-putchar(*format);
-}
-format++;
-}
-va_end(args);
-}
+
 int main(void)
 {
-int number1 = 42;
-int number2 = -123;
-_printf("Number 1: %d\nNumber 2: %i\n", number1, number2);
-return (0);
+	int number1 = 42;
+	int number2 = -123;
+	_printf("Number 1: %d\nNumber 2: %i\n", number1, number2);
+	return (0);
 }
